6c9aaa5734ab305b51cc8b4bfc1c153237fc8446
Sakada Ly <lysakada@gmail.com>
2018-09-03 14:22:16 +0200

    query article les plus visites

--- log_analysis.py
+++ log_analysis.py
@@ -1,6 +1,26 @@
 import psycopg2
 
-select_most_popular_articles = "SELECT distinct name, bio from authors LIMIT 10"
+select_article_by_authors = '''
+  SELECT 
+    authors.name,slug,count(*) 
+  From 
+    articles
+  JOIN 
+    authors
+  ON 
+    articles.author = authors.id 
+  group by authors.name, slug'''
+
+
+select_most_popular_articles = '''
+  SELECT 
+        path, count(*) 
+  FROM 
+        log 
+  WHERE log.status like '200 OK' AND 
+        path like '%/article/%' 
+  GROUP BY path 
+  ORDER BY count  desc;'''
 
 def get_query(select):
 
@@ -11,5 +31,8 @@ def get_query(select):
   database.close()
   return posts
 
+result = get_query(select_article_by_authors)
 
-print (get_query(select_most_popular_articles))
+for select in result:
+  print (select)
+  print ('\n')
