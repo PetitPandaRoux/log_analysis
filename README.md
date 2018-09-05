c74e9aa2758aa595e28cc8919bc3476a10bdc256
Sakada Ly <lysakada@gmail.com>
2018-09-05 15:21:16 +0200

    change in code order

--- README.md
+++ README.md
@@ -24,25 +24,14 @@ SELECT
 ## VIEWS : error,ok and percentage per day
 
 ```SQL
-CREATE VIEW status_per_day AS
+CREATE VIEW error_rate AS
 SELECT
   to_char(time,'Mon dd, yyyy') as day_of_month,
   count(status) filter (WHERE log.status='200 OK') as status_OK,
   count(status) filter (WHERE log.status='404 NOT FOUND') as status_Error,
-  (count(status) filter (WHERE log.status='404 NOT FOUND')::float/( count(status) filter (WHERE log.status='200 OK')+ count(status) filter (WHERE log.status='404 NOT FOUND')))*100 AS error_percent
-  FROM log
-  group by day_of_month;
+  (count(status) filter (WHERE log.status='404 NOT FOUND')::float/( count(status) filter (WHERE log.status='200 OK')+ count(status) filter (WHERE log.status='404 NOT FOUND')))*100 AS error_rate
+FROM 
+  log
+GROUP BY 
+  day_of_month ;
 ```
-
-## Possible select
-SELECT day_of_month, status_OK, status_Error, (status_Error::float/(status_OK::float+status_Error::float))*100 as percent from status_per_day;
-
-.First the product of all tables in the FROM CLAUSE is formed.
-2.The WHERE CLAUSE is then evaluated to eliminate rows that do not satisfy the search_condition.
-3.Next, the rows are grouped using the columns in the GROUP BY CLAUSE.
-4.Then, Groups that do not satisfy the search_condition in the HAVING CLAUSE are eliminated.
-5.Next, the expressions in the SELECT CLAUSE target list are evaluated.
-6.If the DISTINCT keyword in present in the select clause, duplicate rows are now eliminated.
-7.The UNION is taken after each sub-select is evaluated.
-8.Finally, the resulting rows are sorted according to the columns specified in the ORDER BY CLAUSE.
-9.TOP CLAUSE executed
--- log_analysis.py
+++ log_analysis.py
@@ -1,48 +1,47 @@
 import psycopg2
 
-most_popular_articles = '''
-  SELECT views, title, author_name 
-  FROM article_popularity
-  ORDER BY views DESC
-  LIMIT 3;'''
-
-most_popular_author = '''
-  SELECT sum(views) AS total_view, author_name 
-  FROM article_popularity 
-  GROUP BY  author_name
-  ORDER BY total_view desc
-  LIMIT 1;
-  '''
-
-invalid_query = '''
-  SELECT day_of_month 
-  FROM status_per_day 
-  WHERE error_percent>1;
-  '''
-
 def get_query(select):
+    database = psycopg2.connect(dbname="news")
+    cursor = database.cursor()
+    cursor.execute(select)
+    posts = cursor.fetchall()
+    database.close()
+    return posts
 
-  database = psycopg2.connect(dbname="news")
-  cursor = database.cursor()
-  cursor.execute(select)
-  posts = cursor.fetchall()
-  database.close()
-  return posts
+most_read_articles = '''
+    SELECT views, title, author_name 
+    FROM article_popularity
+    ORDER BY views DESC
+    LIMIT 3;'''
 
 result = get_query(most_popular_articles)
 print ("The 3 most visited articles are : ")
 for select in result:
-  print (select),
-  print ('\n'),
+    print (select),
+    print ('\n'),
+
+
+most_popular_author = '''
+    SELECT sum(views) AS total_view, author_name 
+    FROM article_popularity 
+    GROUP BY  author_name
+    ORDER BY total_view desc
+    LIMIT 1;
+    '''
 
 result = get_query(most_popular_author)
 print("The most author read is :")
 for select in result:
-  print (select),
-  print ('\n'),
+    print (select),
+    print ('\n'),
 
+invalid_query = '''
+    SELECT day_of_month 
+    FROM status_per_day 
+    WHERE error_percent>1;
+    '''
 result = get_query(invalid_query)
 print("Days where there is more dans 1% error  :")
 for select in result:
-  print (select),
-  print ('\n'),
+    print (select),
+    print ('\n'),
